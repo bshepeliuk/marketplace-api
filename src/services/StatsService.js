8bceb6dcf99548956eea6f1abd83408bbf9ec177
bshepeliuk <bogdan__sh@ukr.net>
2022-10-23 18:02:08 +0300

    get order stats by month and time

--- src/services/StatsService.js
+++ src/services/StatsService.js
@@ -2,8 +2,9 @@ import getDeviceCategoryStats from '../utils/getDeviceCategoryStats';
 import getOrderCityStats from '../utils/getOrderCityStats';
 import getOrderCustomerStats from '../utils/getOrderCustomerStats';
 import getOrderDeviceStats from '../utils/getOrderDeviceStats';
-import getOrderMonthDayStats from '../utils/getOrderMonthDayStats';
 import getOrderStatusStats from '../utils/getOrderStatusStats';
+import prepareOrderDayMonthStats from '../utils/prepareOrderDayMonthStats';
+import prepareOrderDayTimeStats from '../utils/prepareOrderDayTimeStats';
 import OrderService from './OrderService';
 import TypeService from './TypeService';
 
@@ -18,8 +19,8 @@ const StatsService = {
     const statuses = getOrderStatusStats(orders.rows);
     const customers = getOrderCustomerStats(orders.rows);
     const cities = getOrderCityStats(orders.rows);
-    const dates = getOrderMonthDayStats(orders.rows);
-
+    const orderTime = prepareOrderDayTimeStats(orders.rows);
+    const orderMonth = prepareOrderDayMonthStats(orders.rows);
     // TODO:categories refactoring;
     const result = getDeviceCategoryStats(orders.rows);
     const types = await TypeService.findAll({
@@ -40,7 +41,8 @@ const StatsService = {
       categories,
       customers,
       cities,
-      dates,
+      orderTime,
+      orderMonth,
     };
   },
 };
